fed989b300633dee266b16f9dbb5bfc48c66cc20
bertinmick69 <134129076+bertinmick69@users.noreply.github.com>
2026-05-25 12:30:43 +0200

    Revert "plats"

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import platsRoutes from "./routes/plats.routes.js";
 // TODO : importer vos routes au fur et à mesure
 
 import errorHandler from './middlewares/errorHandler.js';
@@ -18,8 +16,7 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 
 // Exemple avec une route — à dupliquer pour chaque groupe de routes
 app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/plats', platsRoutes);
+
 // TODO : brancher les autres routes ici
 
 // Gestionnaire d'erreurs — toujours EN DERNIER
@@ -29,3 +26,5 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
+
+
